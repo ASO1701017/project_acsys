8fe684f24e9cb890c1934d0f426dc6058909c309
ASO1701019 <1701019@st.asojuku.ac.jp>
2020-06-25 14:30:39 +0900

    fix models/attributes.py Add models/model.py

--- acsys_system/models/attributes.py
+++ acsys_system/models/attributes.py
@@ -1,6 +1,6 @@
 from pynamodb.attributes import UnicodeAttribute, BooleanAttribute, UTCDateTimeAttribute, NumberAttribute, MapAttribute, ListAttribute
 
-class Account(MapAttribute):
+class AccountAttribute(MapAttribute):
     account_ID = NumberAttribute(null=False)
     account_name = UnicodeAttribute(null=False)
     account_height = NumberAttribute(null=False)
@@ -11,21 +11,20 @@ class Account(MapAttribute):
     account_address = UnicodeAttribute(null=False)
     account_pass = UnicodeAttribute(null=False)
 
-class Schedule(MapAttribute):
-    account_ID = NumberAttribute(null=False)
+class ScheduleAttribute(MapAttribute):
+    account_ID = NumberAttribute(of=AccountAttribute)
     schedule_date = UTCDateTimeAttribute(null=False)
     schedule_time = UTCDateTimeAttribute(null=True)
-    circuitmenu_ID = NumberAttribute(null=False)
     calories_burned = NumberAttribute(null=True)
     calories_intake = NumberAttribute(null=True)
     calories_difference = NumberAttribute(null=True)
     ingestion = UnicodeAttribute(null=True)
     training_memory = UnicodeAttribute(null=True)
 
-class Food(MapAttribute):
-    food_name = UnicodeAttribute
-    food_calorie = NumberAttribute
+class FoodAttribute(MapAttribute):
+    food_name = UnicodeAttribute(null=False)
+    food_calorie = NumberAttribute(null=False)
 
-class Motion(MapAttribute):
-    motion_name = UnicodeAttribute
-    motion_calorie = NumberAttribute
+class MotionAttribute(MapAttribute):
+    motion_name = UnicodeAttribute(null=False)
+    motion_calorie = NumberAttribute(null=False)
