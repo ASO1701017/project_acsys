5e5dafb4418f15f5159a374e69d6e8fb684e47ce
ASO1701019 <1701019@st.asojuku.ac.jp>
2020-06-25 21:50:39 +0900

    update settings.py

--- acsys_system/urls.py
+++ acsys_system/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 
-#urlpatterns = [
-#]
+urlpatterns = [
+    path('user/login', views.user, name='login'),
+    path('user/signin', views.user, name='signin'),
+    path('calorie/save_calorie', views.calorie, name='save_calorie'),
+]
