477ee60451114c34cc0ecf61156de28e7cb9f037
raige <raigekka0128@yahoo.co.jp>
2020-06-26 02:40:50 +0900

    +add top.htmlを作成 +update httpレスポンスでテキストを表示する

--- acsys_system/urls.py
+++ acsys_system/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from .views import user
 
 urlpatterns = [
-    path('user/login', views.user, name='login'),
-    path('user/signin', views.user, name='signin'),
-    path('calorie/save_calorie', views.calorie, name='save_calorie'),
+    path('user/', user.index, name='index'),
+    # path('user/login', views.user, name='login'),
+    # path('user/signin', views.user, name='signin'),
+    # path('calorie/save_calorie', views.calorie, name='save_calorie'),
 ]
